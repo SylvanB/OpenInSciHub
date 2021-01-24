2c1f76e644200087ab9c88c870511c8e6e11d542
Sylvan B <sylvanbowdler18@gmail.com>
2021-01-24 14:33:41 +0000

    Use arrow function cause yolo

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.browserAction.onClicked.addListener((tab) => {
     chrome.tabs.create({ url: "https://sci-hub.se/" + tab.url });
 });
