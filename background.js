3d4e2fa1ceb7eaba7a022d98686e6d4e2c8e4cc9
Sylvan B <sylvanbowdler18@gmail.com>
2021-01-24 13:48:32 +0000

    Replaced let with const

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function (tab) {
     // No tabs or host permissions needed!
-    let newURL = "https://sci-hub.se/";
-    let currUrl = tab.url;
+    const newURL = "https://sci-hub.se/";
+    const currUrl = tab.url;
     chrome.tabs.create({ url: newURL + currUrl });
 });
